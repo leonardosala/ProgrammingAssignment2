209ea1b821e42dffc39fd81495bba45c6bca05bb
Leonardo <leonardo.sala@gmail.com>
2017-02-02 12:38:47 +0100

    Added Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For a very large matrix, it may take too long to compute the inverse, 
+## especially if it has to be computed repeatedly (e.g. in a loop). 
+## If the contents of a matrix are not changing, it may make sense to cache the value of the inverse 
+## so that when we need it again, it can be looked up in the cache rather than recomputed. 
+## In this Programming Assignment will take advantage of the scoping rules of the R language 
+## and how they can be manipulated to preserve state inside of an R object
+## Example of usage:
+## > mtx <- matrix(-2:1, 2, 2)
+## > mtx
+##      [,1] [,2]
+## [1,]   -2    0
+## [2,]   -1    1
+## > solve(mtx)
+##     [,1] [,2]
+## [1,] -0.5    0
+## [2,] -0.5    1
+## mtxc <- makeCacheMatrix(mtx)
+## mtxc$get()
+##      [,1] [,2]
+## [1,]   -2    0
+## [2,]   -1    1
+## cacheSolve(mtxc)
+##      [,1] [,2]
+## [1,] -0.5    0
+## [2,] -0.5    1
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that creates a special "matrix" object that can cache its inverse. It includes the following functions:
+##	set the value of the matrix
+##	get the value of the matrix
+##	set the value of the inverse of the matrix
+##	get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 			m 	<- NULL
@@ -18,14 +45,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
             m <- x$getinverse()
             if(!is.null(m)) {
                     message("getting cached data")
-                    return(m)
+ #                   return(m)
             }
             data <- x$get()
             m <- solve(data, ...)
